eed2d5e5b2ecc414f9560b88c6e81de27677fff6
Shantanu Kudva <98833827+ShantanuKudva@users.noreply.github.com>
2023-02-19 15:55:47 +0530

    Update lung_disease_predictor.py

--- lung_disease_predictor.py
+++ lung_disease_predictor.py
@@ -17,9 +17,9 @@ def lung_prediction(input_data):
 
 
     if (prediction[0] == 0):
-        return("The person could have low levels of Lung Cancer")
+        return("The person could have low levels/No signs of Lung Cancer")
     elif (prediction[0] == 1):
-        return("The person could have medium levels of Lung Cancer")
+        return("The person could have moderate levels of Lung Cancer")
     else:
         return("The person could have higher levels of Lung cancer")
 
